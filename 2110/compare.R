15d27621d9edb7264c3ed423373ba46225a64901
marcosgit <marcos-a-g@hotmail.com>
2017-10-29 14:13:59 -0200

    Try

--- 2110/compare.R
+++ 2110/compare.R
@@ -48,185 +48,211 @@ set.seed(10)
 split=0.80
 
 if (!exists("matriz2Gram")) {
-  load("2110/rdas/2gram.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2gram.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  twogram <- treinar(data_train)
-  twogram
-  matriz2Gram <- getMatriz(twogram, data_test)
-  resultados <- addRow(resultados, "2Gram", matriz2Gram)
-  save.image(file="2110/rdas/compare22.RData")
+    twogram <- treinar(data_train)
+    twogram
+    matriz2Gram <- getMatriz(twogram, data_test)
+    resultados <- addRow(resultados, "2Gram", matriz2Gram)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (!exists("matriz2Gram25")) {
-  load("2110/rdas/2gram-25.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2gram-25.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  twogram25 <- treinar(data_train)
-  twogram25
-  matriz2Gram25 <- getMatriz(twogram25, data_test)
-  resultados <- addRow(resultados, "2GRAM 25%", matriz2Gram25)
-  save.image(file="2110/rdas/compare22.RData")
+    twogram25 <- treinar(data_train)
+    twogram25
+    matriz2Gram25 <- getMatriz(twogram25, data_test)
+    resultados <- addRow(resultados, "2GRAM 25%", matriz2Gram25)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (!exists("matriz2GramEntidades")) {
-  load("2110/rdas/2gram-entidades.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2gram-entidades.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  twogramentidades <- treinar(data_train)
-  twogramentidades
-  matriz2GramEntidades <- getMatriz(twogramentidades, data_test)
-  resultados <- addRow(resultados, "2GRAM entidades", matriz2GramEntidades)
-  save.image(file="2110/rdas/compare22.RData")
+    twogramentidades <- treinar(data_train)
+    twogramentidades
+    matriz2GramEntidades <- getMatriz(twogramentidades, data_test)
+    resultados <- addRow(resultados, "2GRAM entidades", matriz2GramEntidades)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (exists("matrizThreeGram")) {
-  load("2110/rdas/3gram.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/3gram.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  threeGram <- treinar(data_train)
-  threeGram
-  matrizThreeGram <- getMatriz(threeGram, data_test)
-  resultados <- addRow(resultados, "3 Gram", matrizThreeGram)
-  save.image(file="2110/rdas/compare22.RData")
+    threeGram <- treinar(data_train)
+    threeGram
+    matrizThreeGram <- getMatriz(threeGram, data_test)
+    resultados <- addRow(resultados, "3 Gram", matrizThreeGram)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (exists("matriz3Gram25")) {
-  load("2110/rdas/3gram-25.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/3gram-25.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  treegram25 <- treinar(data_train)
-  treegram25
-  matriz3Gram25 <- getMatriz(treegram25, data_test)
-  resultados <- addRow(resultados, "3GRAM 25", matriz3Gram25)
-  save.image(file="2110/rdas/compare22.RData")
+    treegram25 <- treinar(data_train)
+    treegram25
+    matriz3Gram25 <- getMatriz(treegram25, data_test)
+    resultados <- addRow(resultados, "3GRAM 25", matriz3Gram25)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (!exists("matriz2GramDBPedia")) {
-  load("2110/rdas/2gram-dbpedia.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2gram-dbpedia.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  twoGDBPedia <- treinar(data_train)
-  twoGDBPedia
-  matriz2GramDBPedia <- getMatriz(twoGDBPedia, data_test)
-  resultados <- addRow(resultados, "2 Gram DBPedia Resource", matriz2GramDBPedia)
-  save.image(file="2110/rdas/compare22.RData")
+    twoGDBPedia <- treinar(data_train)
+    twoGDBPedia
+    matriz2GramDBPedia <- getMatriz(twoGDBPedia, data_test)
+    resultados <- addRow(resultados, "2 Gram DBPedia Resource", matriz2GramDBPedia)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (exists("matrizWikipedia")) {
-  load("2110/rdas/2gram-wikipedia.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2gram-wikipedia.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  wikipediaCategory <- treinar(data_train)
-  wikipediaCategory
-  matrizWikipedia <- getMatriz(wikipediaCategory, data_test)
-  resultados <- addRow(resultados, "2 Gram Wikipedia", matrizWikipedia)
-  save.image(file="2110/rdas/compare22.RData")
+    wikipediaCategory <- treinar(data_train)
+    wikipediaCategory
+    matrizWikipedia <- getMatriz(wikipediaCategory, data_test)
+    resultados <- addRow(resultados, "2 Gram Wikipedia", matrizWikipedia)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (!exists("matrizDBPediaTypesPage")) {
-  load("2110/rdas/2-Gram-dbpedia-types-page-rank.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2-Gram-dbpedia-types-page-rank.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  dbpediaTypesPageRank <- treinar(data_train)
-  dbpediaTypesPageRank
-  matrizDBPediaTypesPage <- getMatriz(dbpediaTypesPageRank, data_test)
-  resultados <- addRow(resultados, "2 Gram dbpedia types page rank", matrizDBPediaTypesPage)
-  save.image(file="2110/rdas/compare22.RData")
+    dbpediaTypesPageRank <- treinar(data_train)
+    dbpediaTypesPageRank
+    matrizDBPediaTypesPage <- getMatriz(dbpediaTypesPageRank, data_test)
+    resultados <- addRow(resultados, "2 Gram dbpedia types page rank", matrizDBPediaTypesPage)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (!exists("matrizDBPediaCompleto")) {
-  load("2110/rdas/2-Gram-dbpedia-types-completo.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2-Gram-dbpedia-types-completo.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  dbpediaTypesCompleto <- treinar(data_train)
-  dbpediaTypesCompleto
-  matrizDBPediaCompleto <- getMatriz(dbpediaTypesCompleto, data_test)
-  resultados <- addRow(resultados, "2 Gram dbpedia types (todos)", matrizDBPediaCompleto)
-  save.image(file="2110/rdas/compare22.RData")
+    dbpediaTypesCompleto <- treinar(data_train)
+    dbpediaTypesCompleto
+    matrizDBPediaCompleto <- getMatriz(dbpediaTypesCompleto, data_test)
+    resultados <- addRow(resultados, "2 Gram dbpedia types (todos)", matrizDBPediaCompleto)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (!exists("matrizTwoGramEntidades2")) {
-  load("2110/rdas/2gram-entidades-25.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2gram-entidades-25.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  twoGramEntidades25 <- treinar(data_train)
-  twoGramEntidades25
-  matrizTwoGramEntidades2 <- getMatriz(twoGramEntidades25, data_test)
-  resultados <- addRow(resultados, "2 Gram 25% + entidades", matrizTwoGramEntidades2)
-  save.image(file="2110/rdas/compare22.RData")
+    twoGramEntidades25 <- treinar(data_train)
+    twoGramEntidades25
+    matrizTwoGramEntidades2 <- getMatriz(twoGramEntidades25, data_test)
+    resultados <- addRow(resultados, "2 Gram 25% + entidades", matrizTwoGramEntidades2)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (exists("matrizTwoGramDBPediaSubject")) {
-  load("2110/rdas/2-Gram-dbpedia-subject.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2-Gram-dbpedia-subject.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  twoGramDBPediaSubject <- treinar(data_train)
-  twoGramDBPediaSubject
-  matrizTwoGramDBPediaSubject <- getMatriz(twoGramDBPediaSubject, data_test)
-  resultados <- addRow(resultados, "2 Gram + subject", matrizTwoGramDBPediaSubject)
-  save.image(file="2110/rdas/compare22.RData")
+    twoGramDBPediaSubject <- treinar(data_train)
+    twoGramDBPediaSubject
+    matrizTwoGramDBPediaSubject <- getMatriz(twoGramDBPediaSubject, data_test)
+    resultados <- addRow(resultados, "2 Gram + subject", matrizTwoGramDBPediaSubject)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (!exists("matrizTwoGram25Hora")) {
-  load("2110/rdas/2gram-25-hora.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2gram-25-hora.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  twoGram25Hora <- treinar(data_train)
-  twoGram25Hora
-  matrizTwoGram25Hora <- getMatriz(twoGram25Hora, data_test)
-  resultados <- addRow(resultados, "2 Gram + 25% + local", matrizTwoGram25Hora)
-  save.image(file="2110/rdas/compare22.RData")
+    twoGram25Hora <- treinar(data_train)
+    twoGram25Hora
+    matrizTwoGram25Hora <- getMatriz(twoGram25Hora, data_test)
+    resultados <- addRow(resultados, "2 Gram + 25% + local", matrizTwoGram25Hora)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 if (!exists("matrizTwoGramCateogoriaLocalizacao")) {
-  load("2110/rdas/2gram-25-categoria-localizacao.Rda")
-  maFinal$resposta <- as.factor(maFinal$resposta)
-  trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
-  data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
-  data_test <- maFinal[-trainIndex,]
+  try({
+    load("2110/rdas/2gram-25-categoria-localizacao.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
 
-  twoGramCategoriaLocalizacao <- treinar(data_train)
-  twoGramCategoriaLocalizacao
-  matrizTwoGramCateogoriaLocalizacao <- getMatriz(twoGramCategoriaLocalizacao, data_test)
-  resultados <- addRow(resultados, "2 Gram + 25% + Categoria da localização", matrizTwoGramCateogoriaLocalizacao)
-  save.image(file="2110/rdas/compare22.RData")
+    twoGramCategoriaLocalizacao <- treinar(data_train)
+    twoGramCategoriaLocalizacao
+    matrizTwoGramCateogoriaLocalizacao <- getMatriz(twoGramCategoriaLocalizacao, data_test)
+    resultados <- addRow(resultados, "2 Gram + 25% + Categoria da localização", matrizTwoGramCateogoriaLocalizacao)
+    save.image(file="2110/rdas/compare22.RData")
+  })
 }
 
 save.image(file="2110/rdas/compare22.RData")
