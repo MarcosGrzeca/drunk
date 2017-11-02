d5f983a874cf05fad16885c070c1ad2aeddab3d9
marcosgit <marcos-a-g@hotmail.com>
2017-11-02 16:17:35 -0200

    Testes

--- 2110/compare.R
+++ 2110/compare.R
@@ -316,10 +316,29 @@ if (!exists("matrizTwoGramEntidadesHoraErro")) {
   })
 }
 
-#save.image(file="2110/rdas/compare22.RData")
+if (!exists("matrizTwoGramPalavroes")) {
+  try({
+    load("2110/rdas/2gram-palavroes.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
+
+    matrizTwoGramPalavroes <- treinar(data_train)
+    twoGramPalavroes
+    matrizTwoGramPalavroes <- getMatriz(twoGramPalavroes, data_test)
+    resultados <- addRow(resultados, "2 Gram + Palavroes", matrizTwoGramPalavroes)
+    save.image(file="2110/rdas/compare22.RData")
+  })
+}
 
 print("FIIMMMMMMMMMMMMMMMMMM")
 
 #load("2110/rdas/compare22.RData")
 #resultados
 #View(resultados)
+
+limpar <- function() {
+  rm("aspectos","aspectosManter","aspectosRemover","dados","dadosQ1","data_test","data_train","dataFrameEntidades","dataFrameEstabelecimento","dataFrameGram","dataFrameHash","dataFrameTexto","dbpediaTypesCompleto","dbpediaTypesPageRank","dtm_train_hash_tags","dtm_train_texto","maFinal","matriz2Gram","matriz2Gram25","matriz2GramDBPedia","matriz2GramEntidades","matriz3Gram25","matrizDBPediaCompleto","matrizDBPediaTypesPage","matrizThreeGram","matrizTwoGram25Hora","matrizTwoGramCateogoriaLocalizacao","matrizTwoGramDBPediaSubject","matrizTwoGramDiaSemana","matrizTwoGramEntidades2","matrizTwoGramEntidadesHora","matrizTwoGramEntidadesHoraErro","matrizTwoGramErros","matrizWikipedia","threeGram","treegram25","twoGDBPedia","twogram","twogram25","twoGram25Hora","twoGramCategoriaLocalizacao","twoGramDBPediaSubject","twoGramDiaSemana","twogramentidades","twoGramEntidades25","twoGramEntidadesHora","twoGramEntidadesHoraErro", "twoGramErros","vectorizerEstabelecimento","vectorizerHashTags","vocab","vocabEstabelecimento","vocabHashTags","wikipediaCategory")
+  save.image(file="2110/rdas/compare22_resultado.RData")
+}
