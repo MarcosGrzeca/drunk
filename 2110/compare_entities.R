563580d7cc1453719d3ba2b4bc66c78ea9aa7c5e
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-11-11 10:43:14 -0200

    Calais

--- 2110/compare_entities.R
+++ 2110/compare_entities.R
@@ -61,6 +61,54 @@ if (!exists("matriz2Calais")) {
   })
 }
 
+if (!exists("matriz2AlchemyEntities")) {
+  try({
+    load("2110/rdas/2gram-entidades-alchemy-entities.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
+
+    twogramAlchemyEntities <- treinar(data_train)
+    twogramAlchemyEntities
+    matriz2AlchemyEntities <- getMatriz(twogramAlchemyEntities, data_test)
+    resultados <- addRow(resultados, "Alchemy Entities", matriz2AlchemyEntities)
+    save.image(file="2110/rdas/compare_entities.RData")
+  })
+}
+
+if (!exists("matriz2AlchemyKeywords")) {
+  try({
+    load("2110/rdas/2gram-entidades-alchemy-keywords.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
+
+    twogramAlchemyKeywords <- treinar(data_train)
+    twogramAlchemyKeywords
+    matriz2AlchemyKeywords <- getMatriz(twogramAlchemyKeywords, data_test)
+    resultados <- addRow(resultados, "Alchemy KeyWords", matriz2AlchemyKeywords)
+    save.image(file="2110/rdas/compare_entities.RData")
+  })
+}
+
+if (!exists("matriz2AlchemyConcepts")) {
+  try({
+    load("2110/rdas/2gram-entidades-alchemy-concepts.Rda")
+    maFinal$resposta <- as.factor(maFinal$resposta)
+    trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+    data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+    data_test <- maFinal[-trainIndex,]
+
+    twogramAlchemyConcepts <- treinar(data_train)
+    twogramAlchemyConcepts
+    matriz2AlchemyConcepts <- getMatriz(twogramAlchemyConcepts, data_test)
+    resultados <- addRow(resultados, "Alchemy Concepts", matriz2AlchemyConcepts)
+    save.image(file="2110/rdas/compare_entities.RData")
+  })
+}
+
 print("FIIMMMMMMMMMMMMMMMMMM")
 limpar <- function() {
   rm("aspectos","aspectosManter","aspectosRemover","dados","dadosQ1","data_test","data_train","dataFrameEntidades","dataFrameEstabelecimento","dataFrameGram","dataFrameHash","dataFrameTexto","dbpediaTypesCompleto","dbpediaTypesPageRank","dtm_train_hash_tags","dtm_train_texto","maFinal","matriz2Gram","matriz2Gram25","matriz2GramDBPedia","matriz2GramEntidades","matriz3Gram25","matrizDBPediaCompleto","matrizDBPediaTypesPage","matrizThreeGram","matrizTwoGram25Hora","matrizTwoGramCateogoriaLocalizacao","matrizTwoGramDBPediaSubject","matrizTwoGramDiaSemana","matrizTwoGramEntidades2","matrizTwoGramEntidadesHora","matrizTwoGramEntidadesHoraErro","matrizTwoGramErros","matrizWikipedia","threeGram","treegram25","twoGDBPedia","twogram","twogram25","twoGram25Hora","twoGramCategoriaLocalizacao","twoGramDBPediaSubject","twoGramDiaSemana","twogramentidades","twoGramEntidades25","twoGramEntidadesHora","twoGramEntidadesHoraErro", "twoGramErros","vectorizerEstabelecimento","vectorizerHashTags","vocab","vocabEstabelecimento","vocabHashTags","wikipediaCategory")
