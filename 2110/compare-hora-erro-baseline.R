e675975ef13e6b468cd501cbcd3ac3176f827ae3
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-12-27 21:23:14 -0200

    Correcao para ler arquivo

--- 2110/compare-hora-erro-baseline.R
+++ 2110/compare-hora-erro-baseline.R
@@ -2,7 +2,7 @@ resultados <- data.frame(matrix(ncol = 4, nrow = 0))
 names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
 
 try({
-#  load("2110/rdas/compare-baseline-cross5.RData")
+  load("2110/rdas/compare-baseline-cross5.RData")
 })
 
 library(tools)
