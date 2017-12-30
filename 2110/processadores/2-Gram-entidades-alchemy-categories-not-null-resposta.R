bcb4d54b8b289f50f675c1835fe66e47feeb680d
MarcosGrzeca <marcos-a-g@hotmail.com>
2017-12-30 16:12:51 -0200

    TESTES

--- 2110/processadores/2-Gram-entidades-alchemy-categories-not-null-resposta.R
+++ 2110/processadores/2-Gram-entidades-alchemy-categories-not-null-resposta.R
@@ -120,6 +120,9 @@ library(RWeka)
 #maFinal <- cbind.fill(dataFrameTexto, dataFrameHash)
 #maFinal <- cbind.fill(maFinal, dataFrameEntidades)
 
-maFinal <- cbind.fill(dataFrameTexto, dataFrameEntidades)
-maFinal <- cbind.fill(maFinal, subset(dados, select = -c(textParser, id, hashtags, textoCompleto, entidades)))
+#maFinal <- cbind.fill(dataFrameTexto, dataFrameEntidades)
+#maFinal <- cbind.fill(maFinal, subset(dados, select = -c(textParser, id, hashtags, textoCompleto, entidades)))
+
+#maFinal <- cbind.fill(dataFrameTexto, dataFrameEntidades)
+maFinal <- cbind.fill(dataFrameTexto, subset(dados, select = -c(textParser, id, hashtags, textoCompleto, entidades)))
 save(maFinal, file = "2110/rdas/2gram-entidades-alchemy-categories-not-null-resposta.Rda")
