9f49d5f0a00c4c97cf1171315c9fd7ef6985ceaa
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-02-07 11:02:56 -0200

    TESTEs com sentimento

---
+++ sentimento/analise_sentimento.R
@@ -0,0 +1,78 @@
+library(tidytext)
+
+#https://www.tidytextmining.com/tidytext.html
+
+sentiments
+
+get_sentiments("afinn")
+
+nrc #TIPOS
+bing #positivo e negativo
+AFINN #indice de polaridade
+
+get_sentiments("nrc")
+
+library(janeaustenr)
+library(dplyr)
+library(stringr)
+
+tidy_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]", 
+                                                 ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  unnest_tokens(word, text)
+
+tidy_books
+
+
+nrcjoy <- get_sentiments("nrc") %>% 
+  filter(sentiment == "joy")
+
+tidy_books %>%
+  filter(book == "Emma") %>%
+  inner_join(nrcjoy) %>%
+  count(word, sort = TRUE)
+
+
+text_df %>%
+  inner_join(nrcjoy) %>%
+  count(word, sort = TRUE)
+
+
+text <- c("Because I could not stop for Death -",
+          "He kindly stopped for me -",
+          "The Carriage held but just Ourselves -",
+          "and Immortality")
+
+text_df <- data_frame(line = 1:4, text = text)
+text_df
+
+library(tidytext)
+
+text_df %>%
+  unnest_tokens(word, text)
+
+
+austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]", 
+                                                 ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  unnest_tokens(word, text)
+
+
+nrcjoy <- get_sentiments("nrc") %>% 
+  filter(sentiment == "joy")
+
+m %>%
+  inner_join(nrcjoy) %>%
+  count(word, sort = TRUE)
+
+m
+m %>%
+  right_join(get_sentiments("nrc")) %>%
+  filter(!is.na(sentiment)) %>%
+  count(sentiment, sort = TRUE)
---
+++ sentimento/presidente.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(tidytext)
+library(glue)
+library(stringr)
+
+# get a list of the files in the input directory
+files <- list.files("sentimento/presidentes")
+files
+
+GetSentiment <- function(file){
+  # get the file
+  fileName <- glue("sentimento/presidentes/", file, sep = "")
+  # get rid of any sneaky trailing spaces
+  fileName <- trimws(fileName)
+  
+  # read in the new file
+  fileText <- glue(read_file(fileName))
+  # remove any dollar signs (they're special characters in R)
+  fileText <- gsub("\\$", "", fileText) 
+  
+  # tokenize
+  tokens <- data_frame(text = fileText) %>% unnest_tokens(word, text)
+  
+  # get the sentiment from the first text: 
+  sentiment <- tokens %>%
+    inner_join(get_sentiments("bing")) %>% # pull out only sentimen words
+    count(sentiment) %>% # count the # of positive & negative words
+    spread(sentiment, n, fill = 0) %>% # made data wide rather than narrow
+    mutate(sentiment = positive - negative) %>% # # of positive words - # of negative owrds
+    mutate(file = file) %>% # add the name of our file
+    mutate(year = as.numeric(str_match(file, "\\d{4}"))) %>% # add the year
+    mutate(president = str_match(file, "(.*?)_")[2]) # add president
+  
+  # return our sentiment dataframe
+  return(sentiment)
+}
+
+# test: should return
+# negative  positive    sentiment   file    year    president
+# 117   240 123 Bush_1989.txt   1989    Bush
+
+sentiments <- data_frame()
+
+# get the sentiments for each file in our datset
+for(i in files){
+  sentiments <- rbind(sentiments, GetSentiment(i))
+}
+
+sentiments
