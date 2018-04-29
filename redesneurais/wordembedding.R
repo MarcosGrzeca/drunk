335fe4b948f983fbab839585bb16ba6f70725eed
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-04-29 14:08:53 -0300

    Redes

--- redesneurais/wordembedding.R
+++ redesneurais/wordembedding.R
@@ -1,10 +1,13 @@
-source(file_path_as_absolute("redesneurais/getDados.R"))
+library(tools)
 library(keras)
+source(file_path_as_absolute("redesneurais/getDados.R"))
 
 maxlen = 20
+max_features = 5000
+outputDim = 16
 
 dados <- getDados()
-data <- processarDados(dados$textParser, maxlen, 5000)
+data <- processarDados(dados$textParser, maxlen, max_features)
 
 labelsTmp <- as.numeric(dados$resposta)
 labels <- as.array(labelsTmp)
@@ -17,22 +20,12 @@ validation_samples <- 799
 source(file_path_as_absolute("redesneurais/separadorDados.R"))
 
 model <- keras_model_sequential() %>%
-  layer_embedding(input_dim = 5000, output_dim = 16,
+  layer_embedding(input_dim = max_features, output_dim = outputDim,
                   input_length = maxlen) %>%
   layer_flatten() %>%
-  layer_dense(units = 5000, activation = "relu") %>%
+  layer_dense(units = max_features, activation = "relu") %>%
   layer_dense(units = 1, activation = "sigmoid")
 
-#model <- keras_model_sequential() %>%
-#  layer_dense(units = 64, activation = "relu", input_shape = c(20)) %>%
-#  layer_dense(units = 64, activation = "relu") %>%
-#  layer_dense(units = 1, activation = "sigmoid")
-
-
-#model <- keras_model_sequential() %>%
-#  layer_dense(units = 16, activation = "relu", input_shape = c(10000)) %>%
-#  layer_dense(units = 16, activation = "relu") %>%
-#  layer_dense(units = 1, activation = "sigmoid")
 
 model %>% compile(
   optimizer = "rmsprop",
@@ -40,17 +33,17 @@ model %>% compile(
   metrics = c("acc")
 )
 
-summary(model)
-
-history <- model %>% fit(
-  x_train, y_train,
-  epochs = 10,
-  batch_size = 64,
-  validation_split = 0.2
-)
-
-history
-
-plot(history)
-
-results <- model %>% evaluate(x_test, y_test)
+tecnica <- "Char NN + dropout"
+testes <- adicionarTeste(3, 16)
+testes <- adicionarTeste(3, 32)
+testes <- adicionarTeste(3, 64)
+testes <- adicionarTeste(5, 16)
+testes <- adicionarTeste(5, 32)
+testes <- adicionarTeste(5, 64)
+testes <- adicionarTeste(7, 16)
+testes <- adicionarTeste(7, 32)
+testes <- adicionarTeste(7, 64)
+testes <- adicionarTeste(10, 16)
+testes <- adicionarTeste(10, 32)
+testes <- adicionarTeste(10, 64)
+source(file_path_as_absolute("redesneurais/parteFinal.R"))
