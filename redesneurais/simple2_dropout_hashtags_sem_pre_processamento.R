0ff65e29a1aa395b4f77d91ada6e5eea6f7df2b3
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-04-29 13:58:37 -0300

    Redes

--- redesneurais/simple2_dropout_hashtags_sem_pre_processamento.R
+++ redesneurais/simple2_dropout_hashtags_sem_pre_processamento.R
@@ -2,15 +2,17 @@ library(keras)
 library(tools)
 source(file_path_as_absolute("redesneurais/getDados.R"))
 maxlen <- 20
+max_features = 5000
+outputDim = 0
 
 dados <- getDados()
-sequences <- processarSequence(dados$textoParserEmoticom, maxlen, 5000)
+sequences <- processarSequence(dados$textoParserEmoticom, maxlen, max_features)
 labelsTmp <- as.numeric(dados$resposta)
 
 training_samples <- 3195
 validation_samples <- 799
 
-vectorize_sequences <- function(sequences, dimension = 5000) {
+vectorize_sequences <- function(sequences, dimension = max_features) {
   results <- matrix(0, nrow = length(sequences), ncol = dimension)
   for (i in 1:length(sequences))
     results[i, sequences[[i]]] <- 1
@@ -30,7 +32,7 @@ x_test <- data[validation_indices,]
 y_test <- labels[validation_indices]
 
 model <- keras_model_sequential() %>%
-  layer_dense(units = 32, activation = "relu", input_shape = c(5000)) %>%
+  layer_dense(units = 32, activation = "relu", input_shape = c(max_features)) %>%
   layer_dropout(rate = 0.2) %>%
   #layer_dense(units = 16, activation = "relu") %>%
   #layer_dropout(rate = 0.2) %>%
@@ -62,17 +64,17 @@ model %>% compile(
   metrics = c("accuracy")
 )
 
-history <- model %>% fit(
-  x_train,
-  y_train,
-  epochs = 5,
-  batch_size = 32,
-  validation_split = 0.2
-)
-
-history
-
-plot(history)
-
-print("FINAL")
-final <- avaliacaoFinal(model, x_test, y_test)
+tecnica <- "Simples 2 (Texto sem pre-processamento) + Hahstags + dropout"
+testes <- adicionarTeste(3, 16)
+testes <- adicionarTeste(3, 32)
+testes <- adicionarTeste(3, 64)
+testes <- adicionarTeste(5, 16)
+testes <- adicionarTeste(5, 32)
+testes <- adicionarTeste(5, 64)
+testes <- adicionarTeste(7, 16)
+testes <- adicionarTeste(7, 32)
+testes <- adicionarTeste(7, 64)
+testes <- adicionarTeste(10, 16)
+testes <- adicionarTeste(10, 32)
+testes <- adicionarTeste(10, 64)
+source(file_path_as_absolute("redesneurais/parteFinal.R"))
