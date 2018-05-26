e13950c7a45fda37e2e900c6d8cfb47d3e9f1225
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-05-26 16:34:28 -0300

    Teste seletores de features

--- 2110/testes2605.R
+++ 2110/testes2605.R
@@ -22,7 +22,7 @@ treinar <- function(data_train){
     fit <- train(x = subset(data_train, select = -c(resposta)),
             y = data_train$resposta, 
             method = "svmLinear", 
-            trControl = trainControl(method = "cv", number = 5, savePred=T),
+            trControl = trainControl(method = "cv", number = 10, savePred=T),
             preProc=c("center"))
     return (fit)
 }
