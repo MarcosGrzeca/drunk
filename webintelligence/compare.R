c5e828bf660a0fd5928b90843aa6d76a78cbf607
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-06-02 20:06:46 -0300

    TEstes

---
+++ webintelligence/compare.R
@@ -0,0 +1,177 @@
+resultados <- data.frame(matrix(ncol = 4, nrow = 0))
+names(resultados) <- c("Baseline", "F1", "Precisão", "Revocação")
+
+try({
+    load("webintelligence/compare.RData")
+})
+
+library(tools)
+library(caret)
+
+if (!require("doMC")) {
+  install.packages("doMC")
+}
+library(doMC)
+library(mlbench)
+
+CORES <- 10
+registerDoMC(CORES)
+
+treinar <- function(data_train){
+    fit <- train(x = subset(data_train, select = -c(resposta)),
+            y = data_train$resposta, 
+            method = "svmLinear", 
+            trControl = trainControl(method = "cv", number = 5, savePred=T),
+            preProc=c("center"))
+    return (fit)
+}
+
+treinarFolds <- function(data_train, numFolds){
+    fit <- train(x = subset(data_train, select = -c(resposta)),
+            y = data_train$resposta, 
+            method = "svmLinear", 
+            trControl = trainControl(method = "cv", number = numFolds, savePred=T),
+            preProc=c("center"))
+    return (fit)
+}
+
+treinarPoly <- function(data_train){
+    fit <- train(x = subset(data_train, select = -c(resposta)),
+            y = data_train$resposta, 
+            method = "svmPoly", 
+            trControl = trainControl(method = "cv", number = 10, savePred=T),
+            preProc=c("center"))
+    return (fit)
+}
+
+getMatriz <- function(fit, data_test) 
+  pred <- predict(fit, subset(data_test, select = -c(resposta)))
+  matriz <- confusionMatrix(data = pred, data_test$resposta, positive="1")
+  return (matriz)
+}
+
+addRow <- function(resultados, baseline, matriz, ...) {
+  print(baseline)
+  newRes <- data.frame(baseline, matriz$byClass["F1"], matriz$byClass["Precision"], matriz$byClass["Recall"])
+  rownames(newRes) <- baseline
+  names(newRes) <- c("Baseline", "F1", "Precisão", "Revocação")
+  newdf <- rbind(resultados, newRes)
+  #save.image(file="webintelligence/compare.RData")
+  return (newdf)
+}
+
+library(magrittr)
+
+set.seed(10)
+split=0.80
+
+if (!exists("matriz2GramEntidadesHoraErro")) {
+  try({
+    for (indice in 1:5){
+      load("2110/rdas/2gram-entidades-hora-erro-q2-not-null.Rda")
+      maFinal$resposta <- as.factor(maFinal$resposta)
+      trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+      data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+      data_test <- maFinal[-trainIndex,]
+
+      twogramEntidadesHoraErro <- treinar(data_train)
+      twogramEntidadesHoraErro
+      matriz2GramEntidadesHoraErro <- getMatriz(twogramEntidadesHoraErro, data_test)
+      resultados <- addRow(resultados, "2 GRAM + entidades + hora + erro", matriz2GramEntidadesHoraErro)
+      save.image(file="webintelligence/compare.RData")
+    }
+  })
+}
+
+if (!exists("matriz2GramEntidadesHoraErroPoly")) {
+  try({
+    for (indice in 1:5){
+      load("2110/rdas/2gram-entidades-hora-erro-q2-not-null.Rda")
+      maFinal$resposta <- as.factor(maFinal$resposta)
+      trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+      data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+      data_test <- maFinal[-trainIndex,]
+
+      twogramEntidadesHoraErro <- treinar(data_train)
+      twogramEntidadesHoraErro
+      matriz2GramEntidadesHoraErroPoly <- getMatriz(twogramEntidadesHoraErro, data_test)
+      resultados <- addRow(resultados, "2 GRAM + entidades + hora + erro (Poly)", matriz2GramEntidadesHoraErroPoly)
+      save.image(file="webintelligence/compare.RData")
+    }
+  })
+}
+
+if (!exists("matriz3Gram")) {
+  try({
+    for (indice in 1:5) {
+      load("2110/rdas/3gram-25-q2-v2-not-null.Rda")
+      maFinal$resposta <- as.factor(maFinal$resposta)
+      trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+      data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+      data_test <- maFinal[-trainIndex,]
+
+      threeGram25 <- treinar(data_train)
+      threeGram25
+      matriz3Gram <- getMatriz(threeGram25, data_test)
+      resultados <- addRow(resultados, "3 Gram + 25% + Bow #", matriz3Gram)
+      }
+  })
+}
+
+if (!exists("matriz3GramPoly")) {
+  try({
+    for (indice in 1:5) {
+      load("2110/rdas/3gram-25-q2-v2-not-null.Rda")
+      maFinal$resposta <- as.factor(maFinal$resposta)
+      trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+      data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+      data_test <- maFinal[-trainIndex,]
+
+      threeGram25 <- treinar(data_train)
+      threeGram25
+      matriz3GramPoly <- getMatriz(threeGram25, data_test)
+      resultados <- addRow(resultados, "3 Gram + 25% + Bow # (Poly)", matriz3GramPoly)
+      }
+  })
+}
+
+if (!exists("matrizTwoGramTypesInfoQ2Entidades")) {
+  try({
+    for (indice in 1:5){
+      load("2110/rdas/2-Gram-dbpedia-types-enriquecimento-info-q2-not-null.Rda")
+      maFinal$resposta <- as.factor(maFinal$resposta)
+      trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+      data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+      data_test <- maFinal[-trainIndex,]
+
+      twoGramTypesCFS <- treinar(data_train)
+      twoGramTypesCFS
+      matrizTwoGramTypesInfoQ2Entidades <- getMatriz(twoGramTypesCFS, data_test)
+      resultados <- addRow(resultados, "2 Gram + Types + entidades + Info Gain Q2", matrizTwoGramTypesInfoQ2Entidades)
+      save.image(file="2110/rdas/testes2705.RData")
+    }
+  })
+}
+
+if (!exists("matrizTwoGramTypesInfoQ2EntidadesPolyV2")) {
+  try({
+    for (indice in 1:5){
+      load("2110/rdas/2-Gram-dbpedia-types-enriquecimento-info-q2-not-null.Rda")
+      maFinal$resposta <- as.factor(maFinal$resposta)
+      trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+      data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+      data_test <- maFinal[-trainIndex,]
+
+      twoGramTypesCFS <- treinarPoly(data_train)
+      twoGramTypesCFS
+      matrizTwoGramTypesInfoQ2EntidadesPolyV2 <- getMatriz(twoGramTypesCFS, data_test)
+      resultados <- addRow(resultados, "2 Gram + Types + entidades + Info Gain Q2 (Poly)", matrizTwoGramTypesInfoQ2EntidadesPolyV2)
+      save.image(file="2110/rdas/testes2705.RData")
+    }
+  })
+}
+
+limpar <- function() {
+  rm("aspectos","aspectosManter","aspectosRemover","dados","dadosQ1","data_test","data_train","dataFrameEntidades","dataFrameEstabelecimento","dataFrameGram","dataFrameHash","dataFrameTexto","dbpediaTypesCompleto","dbpediaTypesPageRank","dtm_train_hash_tags","dtm_train_texto","maFinal","matriz2Gram","matriz2Gram25","matriz2GramDBPedia","matriz2GramEntidades","matriz3Gram25","matrizDBPediaCompleto","matrizDBPediaTypesPage","matrizThreeGram","matrizTwoGram25Hora","matrizTwoGramCateogoriaLocalizacao","matrizTwoGramDBPediaSubject","matrizTwoGramDiaSemana","matrizTwoGramEntidades2","matrizTwoGramEntidadesHora","matrizTwoGramEntidadesHoraErro","matrizTwoGramErros","matrizWikipedia","threeGram","treegram25","twoGDBPedia","twogram","twogram25","twoGram25Hora","twoGramCategoriaLocalizacao","twoGramDBPediaSubject","twoGramDiaSemana","twogramentidades","twoGramEntidades25","twoGramEntidadesHora","twoGramEntidadesHoraErro", "twoGramErros","vectorizerEstabelecimento","vectorizerHashTags","vocab","vocabEstabelecimento","vocabHashTags","wikipediaCategory")
+  save.image(file="2110/rdas/compare22_resultado.RData")
+}
