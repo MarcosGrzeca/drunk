daa7499ab4ee6b6c6f3d473af4cc7379a0ae8d7e
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-06-02 21:06:15 -0300

    TEstes

--- webintelligence/compare.R
+++ webintelligence/compare.R
@@ -39,7 +39,7 @@ treinarPoly <- function(data_train){
     fit <- train(x = subset(data_train, select = -c(resposta)),
             y = data_train$resposta, 
             method = "svmPoly", 
-            trControl = trainControl(method = "cv", number = 10, savePred=T),
+            trControl = trainControl(method = "cv", number = 5, savePred=T),
             preProc=c("center"))
     return (fit)
 }
@@ -114,6 +114,7 @@ if (!exists("matriz3Gram")) {
       threeGram25
       matriz3Gram <- getMatriz(threeGram25, data_test)
       resultados <- addRow(resultados, "3 Gram + 25% + Bow #", matriz3Gram)
+      save.image(file="webintelligence/compare.RData")
       }
   })
 }
@@ -131,6 +132,7 @@ if (!exists("matriz3GramPoly")) {
       threeGram25
       matriz3GramPoly <- getMatriz(threeGram25, data_test)
       resultados <- addRow(resultados, "3 Gram + 25% + Bow # (Poly)", matriz3GramPoly)
+      save.image(file="webintelligence/compare.RData")
       }
   })
 }
@@ -148,7 +150,7 @@ if (!exists("matrizTwoGramTypesInfoQ2Entidades")) {
       twoGramTypesCFS
       matrizTwoGramTypesInfoQ2Entidades <- getMatriz(twoGramTypesCFS, data_test)
       resultados <- addRow(resultados, "2 Gram + Types + entidades + Info Gain Q2", matrizTwoGramTypesInfoQ2Entidades)
-      save.image(file="2110/rdas/testes2705.RData")
+      save.image(file="webintelligence/compare.RData")
     }
   })
 }
@@ -166,10 +168,47 @@ if (!exists("matrizTwoGramTypesInfoQ2EntidadesPolyV2")) {
       twoGramTypesCFS
       matrizTwoGramTypesInfoQ2EntidadesPolyV2 <- getMatriz(twoGramTypesCFS, data_test)
       resultados <- addRow(resultados, "2 Gram + Types + entidades + Info Gain Q2 (Poly)", matrizTwoGramTypesInfoQ2EntidadesPolyV2)
-      save.image(file="2110/rdas/testes2705.RData")
+      save.image(file="webintelligence/compare.RData")
     }
   })
 }
+
+if (!exists("matrizTwoGramTypesInfoQ2EntidadesEnriquecimentoEPoda")) {
+  try({
+    for (indice in 1:5){
+      load("2110/rdas/2-Gram-dbpedia-types-enriquecimento-info-q2-not-null_info_entidades.Rda")
+      maFinal$resposta <- as.factor(maFinal$resposta)
+      trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+      data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+      data_test <- maFinal[-trainIndex,]
+
+      twoGramTypesCFS <- treinar(data_train)
+      twoGramTypesCFS
+      matrizTwoGramTypesInfoQ2EntidadesEnriquecimentoEPoda <- getMatriz(twoGramTypesCFS, data_test)
+      resultados <- addRow(resultados, "2 Gram + Types (Info Gain) + Entidades (Info Gain) + Q2", matrizTwoGramTypesInfoQ2EntidadesEnriquecimentoEPoda)
+      save.image(file="webintelligence/compare.RData")
+    }
+  })
+}
+
+if (!exists("matrizTwoGramTypesInfoQ2EntidadesEnriquecimentoEPodaPoly")) {
+  try({
+    for (indice in 1:5){
+      load("2110/rdas/2-Gram-dbpedia-types-enriquecimento-info-q2-not-null_info_entidades.Rda")
+      maFinal$resposta <- as.factor(maFinal$resposta)
+      trainIndex <- createDataPartition(maFinal$resposta, p=split, list=FALSE)
+      data_train <- as.data.frame(unclass(maFinal[ trainIndex,]))
+      data_test <- maFinal[-trainIndex,]
+
+      twoGramTypesCFS <- treinarPoly(data_train)
+      twoGramTypesCFS
+      matrizTwoGramTypesInfoQ2EntidadesEnriquecimentoEPodaPoly <- getMatriz(twoGramTypesCFS, data_test)
+      resultados <- addRow(resultados, "2 Gram + Types (Info Gain) + Entidades (Info Gain) + Q2 (Poly)", matrizTwoGramTypesInfoQ2EntidadesEnriquecimentoEPodaPoly)
+      save.image(file="webintelligence/compare.RData")
+    }
+  })
+}
+
 
 limpar <- function() {
   rm("aspectos","aspectosManter","aspectosRemover","dados","dadosQ1","data_test","data_train","dataFrameEntidades","dataFrameEstabelecimento","dataFrameGram","dataFrameHash","dataFrameTexto","dbpediaTypesCompleto","dbpediaTypesPageRank","dtm_train_hash_tags","dtm_train_texto","maFinal","matriz2Gram","matriz2Gram25","matriz2GramDBPedia","matriz2GramEntidades","matriz3Gram25","matrizDBPediaCompleto","matrizDBPediaTypesPage","matrizThreeGram","matrizTwoGram25Hora","matrizTwoGramCateogoriaLocalizacao","matrizTwoGramDBPediaSubject","matrizTwoGramDiaSemana","matrizTwoGramEntidades2","matrizTwoGramEntidadesHora","matrizTwoGramEntidadesHoraErro","matrizTwoGramErros","matrizWikipedia","threeGram","treegram25","twoGDBPedia","twogram","twogram25","twoGram25Hora","twoGramCategoriaLocalizacao","twoGramDBPediaSubject","twoGramDiaSemana","twogramentidades","twoGramEntidades25","twoGramEntidadesHora","twoGramEntidadesHoraErro", "twoGramErros","vectorizerEstabelecimento","vectorizerHashTags","vocab","vocabEstabelecimento","vocabHashTags","wikipediaCategory")
