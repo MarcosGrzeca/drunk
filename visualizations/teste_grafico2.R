14218754540916436e20b06ee2e8d7f76a59f475
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-06-03 15:00:34 -0300

    Vas

--- visualizations/teste_grafico2.R
+++ visualizations/teste_grafico2.R
@@ -1,29 +1,15 @@
 library(ggplot2)
 df1 <- data.frame(supp=rep(c("Baseline", "Proposed Framework"), each=3),
-                  metric=rep(c("F1-score", "Precision", "Recall"),2),
-                  len=c(89.309, 88.913, 89.709, 91.029, 90.131, 91.946))
-
-df2 <- data.frame(supp=rep(c("Baseline", "Proposed Framework"), each=3),
-                  metric=rep(c("F1-score", "Precision", "Recall"),2),
-                  len=c(82.562, 80.555, 84.671, 87.755, 82.165, 94.160))
-
-df3 <- data.frame(supp=rep(c("Baseline", "Proposed Framework"), each=3),
-                  metric=rep(c("F1-score", "Precision", "Recall"),2),
-                  len=c(76.470, 73.239, 80, 80.141, 74.342, 86.923))
-
-
-library(ggplot2)
-df1 <- data.frame(supp=rep(c("Baseline", "Proposed Framework"), each=3),
                   metric=rep(c("Recall", "Precision", "F1-Measure"),2),
                   len=c(89.709, 88.913, 89.309, 91.946, 90.131, 91.029))
 
 df2 <- data.frame(supp=rep(c("Baseline", "Proposed Framework"), each=3),
                   metric=rep(c("Recall", "Precision", "F1-Measure"),2),
-                  len=c(84.671, 80.555, 82.562, 94.160, 82.165, 87.755))
+                  len=c(89.343, 81.663, 85.503, 96.131, 80.733, 88.432))
 
 df3 <- data.frame(supp=rep(c("Baseline", "Proposed Framework"), each=3),
                   metric=rep(c("Recall", "Precision", "F1-Measure"),2),
-                  len=c(80.000, 73.239, 76.470, 86.923, 74.342, 80.141))
+                  len=c(81.384, 76.191, 78.787, 95.182, 80.892, 88.037))
 
 head(df1)
 #df1$metric=as.factor(df1$metric)
@@ -34,7 +20,7 @@ df3$metric <- factor(df3$metric, levels = c("Recall", "Precision", "F1-Measure")
 
 
 
-ggplot(data=df3, aes(x=metric, y=len, fill=supp)) +
+ggplot(data=df2, aes(x=metric, y=len, fill=supp)) +
   geom_bar(stat="identity", position=position_dodge()) +
   geom_text(aes(label=len), vjust=1.6, color="white",
           position = position_dodge(0.9), size=3.5) +
@@ -47,8 +33,6 @@ df1 %>%
   mutate(name = fct_relevel(name, "Recall", "Precision", "F1-Measure")) %>%
   ggplot( aes(x=name, y=val)) +
   geom_bar(stat="identity")
-
-
 
 
 
