ee0236e9161a09e039eb143a2535e8d0de84fb32
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-07-12 22:38:02 -0300

    Pré classificar tweets

---
+++ pre_selecionar/train.R
@@ -0,0 +1,40 @@
+library(tools)
+library(caret)
+
+if (!require("doMC")) {
+  install.packages("doMC")
+}
+library(doMC)
+library(mlbench)
+
+CORES <- 4
+registerDoMC(CORES)
+
+treinar <- function(data_train){
+    fit <- train(x = subset(data_train, select = -c(resposta)),
+            y = data_train$resposta, 
+            method = "svmLinear", 
+            trControl = trainControl(method = "cv", number = 5, savePred=T))
+    return (fit)
+}
+
+treinarPoly <- function(data_train){
+    # registerDoMC(CORES)
+    fit <- train(x = subset(data_train, select = -c(resposta)),
+            y = data_train$resposta, 
+            method = "svmPoly", 
+            trControl = trainControl(method = "cv", number = 5, savePred=T))
+    return (fit)
+}
+
+library(magrittr)
+
+set.seed(10)
+split=0.80
+
+try({
+	load("2110/rdas/2gram-q2-not-null.Rda")
+	model <- treinarPoly(maFinal)
+	saveRDS(model, "model_q2.rds")
+	#my_model <- readRDS("model.rds")
+})
